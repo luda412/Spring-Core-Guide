8e588f1bab425c1e27d5da27d1c4c83233c0ad3c
luda412 <201810791@sangmyung.kr>
2024-11-23 16:45:48 +0900

    [Feat]- RequestParam GetMapping version1&2, version2 - use Map<Key, Value>

--- src/main/java/com/springcoreguide/hello/controller/GetController.java
+++ src/main/java/com/springcoreguide/hello/controller/GetController.java
@@ -1,9 +1,8 @@
 package com.springcoreguide.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -16,5 +15,28 @@ public class GetController {
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
         return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2 (@PathVariable("variable") String var){
+        return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ){
+        return name +" " + email + " "+ organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : "+map.getValue()+"\n");
+        });
+        return sb.toString();
     }
 }
