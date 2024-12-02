1eb666ac855cb1fdfb7399cd235c6bb9e38b993d
luda412 <201810791@sangmyung.kr>
2024-12-02 17:14:26 +0900

    [Chore] - Product Entity 생성

---
+++ src/main/java/com/springcoreguide/hello/data/Product.java
@@ -0,0 +1,31 @@
+package com.springcoreguide.hello.data;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createAt;
+
+    private LocalDateTime updateAt;
+}
