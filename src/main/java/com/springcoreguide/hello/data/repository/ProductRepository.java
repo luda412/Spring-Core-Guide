0b738b67fbe4ab091a5a35bd3d0aa5e354481db7
luda412 <201810791@sangmyung.kr>
2024-12-03 14:06:25 +0900

    [Feat] - ProductRepository 생성

---
+++ src/main/java/com/springcoreguide/hello/data/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springcoreguide.hello.data.repository;
+
+import com.springcoreguide.hello.data.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
