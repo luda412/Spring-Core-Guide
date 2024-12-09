c6327bc8ceff020270e99e2f44c0be3045db971d
luda412 <201810791@sangmyung.kr>
2024-12-09 16:35:32 +0900

    [Feat] - Add ChangeProductNameDto & ProductController | Feat Get, Post, Put, Delete Method

---
+++ src/main/java/com/springcoreguide/hello/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.springcoreguide.hello.controller;
+
+import com.springcoreguide.hello.dto.ChangeProductNameDto;
+import com.springcoreguide.hello.dto.ProductDto;
+import com.springcoreguide.hello.dto.ProductResponseDto;
+import com.springcoreguide.hello.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<ProductResponseDto> getProduct(Long number){
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto){
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PutMapping()
+    public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductNameDto changeProductNameDto) throws Exception{
+        ProductResponseDto productResponseDto = productService.changeProductName(changeProductNameDto.getNumber(), changeProductNameDto.getName());
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception{
+        productService.deleteProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
+    }
+}
---
+++ src/main/java/com/springcoreguide/hello/dto/ChangeProductNameDto.java
@@ -0,0 +1,11 @@
+package com.springcoreguide.hello.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangeProductNameDto {
+    private Long number;
+    private String name;
+}
