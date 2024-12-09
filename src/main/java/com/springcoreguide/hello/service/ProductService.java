d7d0cdcfa4311707373e24d1f7671daf0b392252
luda412 <201810791@sangmyung.kr>
2024-12-09 16:14:27 +0900

    [Feat] - Service interface 생성 및 메서드 구현체 구현

--- src/main/java/com/springcoreguide/hello/service/ProductService.java
+++ src/main/java/com/springcoreguide/hello/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.springcoreguide.hello.service;
 
+import com.springcoreguide.hello.dto.ProductDto;
+import com.springcoreguide.hello.dto.ProductResponseDto;
+
 public interface ProductService {
+    ProductResponseDto getProduct(Long number);
+
+    ProductResponseDto saveProduct(ProductDto productDto);
+
+    ProductResponseDto changeProductName(Long number, String name) throws Exception;
+
+    void deleteProduct(Long number) throws Exception;
 }
